9bd0abf54d0a3b68d4e09b04d548009e52c6dcf7
Jieying Yang <jieying.yang@cca.edu>
2021-10-29 16:49:01 -0700

    finished the todo list_v1.01

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 var items =["Buy milk", "Buy bread"];
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
