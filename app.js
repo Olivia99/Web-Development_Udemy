e51c51ce2a383982b8b8c144c2543b9559a42e00
Jieying Yang <jieying.yang@cca.edu>
2021-10-29 15:50:57 -0700

    created todo list

--- app.js
+++ app.js
@@ -4,43 +4,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items =["Buy milk", "Buy bread"];
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
 
 var today = new Date();
-  var currentDay = today.getDay();
-  var day = "" ;
 
-switch (currentDay) {
-  case 0:
-    day = "Sunday";
-    break;
-    case 1:
-    day = "Monday";
-    break;
-    case 2:
-    day = "Tuesday";
-    break;
-    case 3:
-    day = "Wednesday";
-    break;
-    case 4:
-    day = "Thursday";
-    break;
-    case 5:
-    day = "Friday";
-    break;
-    case 6:
-    day = "Saturday";
-    break;
-  default:
-    console.log("Error to currentDay value is " + currentDay);
-}
+var option = {
+  weekday:"long",
+  day: "numeric",
+  month:"long"
+};
+
+var day = today.toLocaleDateString("en-US", option);
 
 
-res.render("list", {day:day});
+
+res.render("list", {day:day, newItem: items});
+
+});
+
+app.post("/", function(req, res){
+
+var item = req.body.todoItem;
+
+items.push(item);
+console.log(items);
+res.redirect("/");
+
 
 });
 
